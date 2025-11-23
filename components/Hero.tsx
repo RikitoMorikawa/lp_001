2f21cd52443fe9b261bd2bd6d154dcdc5d5df04d
RikitoMorikawa <rikitomorikawa.unity@gmail.com>
2025-11-23 22:42:04 +0900

    ＃

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,7 +32,7 @@ const Hero: React.FC = () => {
       </div>
 
       {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce text-white/80 flex justify-center w-full">
+      <div className="absolute inset-x-0 bottom-8 flex justify-center animate-bounce text-white/80">
         <ChevronDown size={40} />
       </div>
     </div>
